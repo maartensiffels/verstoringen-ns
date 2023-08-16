04e04ac0765f9c1daf6a22bc1e96ce145583600f
Just Vervaart <35161717+justusvervaart@users.noreply.github.com>
2023-08-16 09:36:38 +0200

    Update scrape_verstoringen_ns.py
    Advies toegevoegd in een nieuwe kolom

--- scrape_verstoringen_ns.py
+++ scrape_verstoringen_ns.py
@@ -4,14 +4,34 @@ import os
 from datetime import datetime
 import pytz
 
+def get_unique_station_codes(disruption):
+    station_codes = set()
+    publication_sections = disruption.get("publicationSections", [])
+
+    for section in publication_sections:
+        for station in section.get("section", {}).get("stations", []):
+            station_codes.add(station["stationCode"])
+
+    return ", ".join(station_codes)
+
+def get_unique_station_names(disruption):
+    station_names = set()
+    publication_sections = disruption.get("publicationSections", [])
+
+    for section in publication_sections:
+        for station in section.get("section", {}).get("stations", []):
+            station_names.add(station["name"])
+
+    return ", ".join(station_names)
+
 def get_unique_consequence_descriptions(disruption):
     descriptions = set()
     publication_sections = disruption.get("publicationSections", [])
-    
+
     for section in publication_sections:
         if "consequence" in section and "description" in section["consequence"]:
             descriptions.add(section["consequence"]["description"])
-            
+
     return ", ".join(descriptions)
 
 def fetch_disruptions():
@@ -19,20 +39,20 @@ def fetch_disruptions():
     headers = {
         "Ocp-Apim-Subscription-Key": "dd6c3e5f733d48b59a7c83abe2161a2f"
     }
-    
+
     response = requests.get(url, headers=headers)
     return response.json()
 
 def save_to_csv(data, year):
     filename = f"verstoringen-dienstregeling-ns-{year}.csv"
     file_exists = os.path.isfile(filename)
-    
+
     with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
-        
+
         if not file_exists:
             writer.writerow(["ID", "Titel", "Fase ID", "Fase Label", "Oorzaak", "Impact", "Gevolg", "Beschrijving", "Minimale Extra Reistijd (min.)", "Maximale Extra Reistijd (min.)", "Station Codes", "Station Namen", "Starttijd", "Geschatte Eindtijd", "Eindtijd", "Tijdstip collectie"])
-            
+
         for item in data:
             writer.writerow([item["ID"], item["Titel"], item["Fase ID"], item["Fase Label"], item["Oorzaak"], item["Impact"], item["Gevolg"], item["Beschrijving"], item["Minimale Extra Reistijd"], item["Maximale Extra Reistijd"], item["Station Codes"], item["Station Namen"], item["Starttijd"], item["Geschatte Eindtijd"], item["Eindtijd"], item["Tijdstip collectie"]])
 
@@ -53,11 +73,12 @@ filtered_disruptions = [
         "Oorzaak": disruption.get("timespans", [{}])[0].get("cause", {}).get("label", ""),
         "Impact": disruption.get("impact", {}).get("value", ""),
         "Gevolg": get_unique_consequence_descriptions(disruption),
+        "Advies": disruption.get("timespans", [{}])[0].get("alternativeTransport", {}).get("label", ""),
         "Beschrijving": disruption.get("timespans", [{}])[0].get("situation", {}).get("label", ""),
         "Minimale Extra Reistijd": disruption.get("summaryAdditionalTravelTime", {}).get("minimumDurationInMinutes", ""),
         "Maximale Extra Reistijd": disruption.get("summaryAdditionalTravelTime", {}).get("maximumDurationInMinutes", ""),
-        "Station Codes": ", ".join([station.get("stationCode", "") for station in disruption.get("publicationSections", [{}])[0].get("section", {}).get("stations", [])]),
-        "Station Namen": ", ".join([station.get("name", "") for station in disruption.get("publicationSections", [{}])[0].get("section", {}).get("stations", [])]),
+        "Station Codes": get_unique_station_codes(disruption),
+        "Station Namen": get_unique_station_names(disruption),
         "Starttijd": disruption.get("start", "").replace('+0200', ''),
         "Geschatte Eindtijd": disruption.get("expectedDuration", {}).get("endTime", "").replace('+0200', ''),
         "Eindtijd": disruption.get("end", "").replace('+0200', ''),
@@ -68,4 +89,4 @@ filtered_disruptions = [
 
 save_to_csv(filtered_disruptions, current_year)
 for item in filtered_disruptions:
-    print(f'ID: {item["ID"]}, Titel: {item["Titel"]}, Fase ID: {item["Fase ID"]}, Fase Label: {item["Fase Label"]}, Oorzaak: {item["Oorzaak"]}, Impact: {item["Impact"]}, Gevolg: {item["Gevolg"]}, Beschrijving: {item["Beschrijving"]}, Minimale Extra Reistijd: {item["Minimale Extra Reistijd"]}, Maximale Extra Reistijd: {item["Maximale Extra Reistijd"]}, Station Codes: {item["Station Codes"]}, Station Namen: {item["Station Namen"]}, Starttijd: {item["Starttijd"]}, Geschatte Eindtijd: {item["Geschatte Eindtijd"]}, Eindtijd: {item["Eindtijd"]}, Tijdstip collectie: {item["Tijdstip collectie"]}')
+    print(f'ID: {item["ID"]}, Titel: {item["Titel"]}, Fase ID: {item["Fase ID"]}, Fase Label: {item["Fase Label"]}, Oorzaak: {item["Oorzaak"]}, Advies: {item["Advies"]}, Impact: {item["Impact"]}, Gevolg: {item["Gevolg"]}, Beschrijving: {item["Beschrijving"]}, Minimale Extra Reistijd: {item["Minimale Extra Reistijd"]}, Maximale Extra Reistijd: {item["Maximale Extra Reistijd"]}, Station Codes: {item["Station Codes"]}, Station Namen: {item["Station Namen"]}, Starttijd: {item["Starttijd"]}, Geschatte Eindtijd: {item["Geschatte Eindtijd"]}, Eindtijd: {item["Eindtijd"]}, Tijdstip collectie: {item["Tijdstip collectie"]}')
